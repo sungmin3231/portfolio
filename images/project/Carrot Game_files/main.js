f1243b45948f80e4a37ef53d4a52674616097f0e
sungmin3231 <psungmin88.1@gmail.com>
2020-12-15 18:08:39 +0900

    img 추가 수정

---
+++ images/project/Carrot Game_files/main.js
@@ -0,0 +1,37 @@
+"use strict";
+
+import PopUp from "./popup.js";
+import * as sound from "./sound.js";
+import { GameBuilder, Reason } from "./game.js";
+
+const gameFinishBanner = new PopUp();
+const game = new GameBuilder()
+  .withGameDuration(5)
+  .withCarrotCount(3)
+  .withBugCount(3)
+  .build();
+
+game.setGameStopListener(reason => {
+  let message;
+  switch (reason) {
+    case Reason.cancel:
+      message = "Replay❓";
+      sound.playAlert();
+      break;
+    case Reason.win:
+      message = "You Won 🎉";
+      sound.playWin();
+      break;
+    case Reason.lose:
+      message = "You Lost 🤪";
+      sound.playBug();
+      break;
+    default:
+      throw new Error("not valid reason");
+  }
+  gameFinishBanner.showWithText(message);
+});
+
+gameFinishBanner.setClickListener(() => {
+  game.start();
+});
